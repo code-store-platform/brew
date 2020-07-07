2eaf773ccc6bf845d4e5ae49b7e3cdf9b73ecaba
valcker <arthur.murauskas@gmail.com>
2020-07-07 14:08:56 +0200

    Set node version to LTS

--- Formula/codestore.rb
+++ Formula/codestore.rb
@@ -4,7 +4,7 @@ class Codestore < Formula
   url "https://s3.code.store/codestore-v1.2.12/codestore-v1.2.12.tar.gz"
   sha256 "7474ac084e12cdd946b30e2eed7e5689cfbc656ff19e53f947141e67fa3c0d28"
   license "MIT"
-  depends_on "node"
+  depends_on "node@12"
 
   def install
     inreplace "bin/codestore", /^CLIENT_HOME=/, "export CODESTORE_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
