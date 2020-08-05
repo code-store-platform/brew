9e801ef3fe87b53b0793dffe1ec77f3e7787f796
valcker <arthur.murauskas@gmail.com>
2020-08-05 20:33:39 +0200

    Replace archive with a standalone one

--- Formula/codestore.rb
+++ Formula/codestore.rb
@@ -1,8 +1,8 @@
 class Codestore < Formula
   desc "Command-line interface of code.store"
   homepage "https://code.store/"
-  url "https://s3.code.store/codestore-v1.4.9/codestore-v1.4.9.tar.gz"
-  sha256 "765cdbf7e375c01d103317e76d0df589e04762ab59c140e375ce788c90b36085"
+  url "https://s3.code.store/codestore-v1.4.9/codestore-v1.4.9-darwin-x64.tar.gz"
+  sha256 "02b83c6777c4cb8f63c7fd22bf27821299993f9ae6933a3f207bad5c4012a568"
   license "MIT"
   depends_on "node@12"
 
