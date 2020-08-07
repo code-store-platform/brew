83446d98bc4cc4c1ea3ac0973fbe76689f00c42c
art-yushkov <64585241+art-yushkov@users.noreply.github.com>
2020-08-07 11:31:24 +0300

    Update codestore.rb

--- Formula/codestore.rb
+++ Formula/codestore.rb
@@ -2,8 +2,8 @@ class Codestore < Formula
   desc "Command-line interface of code.store"
   homepage "https://code.store/"
   url "https://s3.code.store/codestore-v1.4.11/codestore-v1.4.11-darwin-x64.tar.gz"
-  sha256 "567d15662186b6a80d94522b4cf8f43a715d4b974f3675bd3c9a44e4ebf6476e"
   version "1.4.11"
+  sha256 "567d15662186b6a80d94522b4cf8f43a715d4b974f3675bd3c9a44e4ebf6476e"
   license "MIT"
   depends_on "node@12"
 
