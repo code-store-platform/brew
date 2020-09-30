6c84421f62ac16cc4d634c1d194533b0c52671f1
codeStoreGitHub <accounts.main@code.store>
2020-09-30 13:13:16 +0000

    codestore v1.18.27

--- Formula/codestore.rb
+++ Formula/codestore.rb
@@ -1,9 +1,9 @@
 class Codestore < Formula
   desc "Command-line interface of code.store"
   homepage "https://code.store/"
-  url "https://s3.code.store/codestore-v1.18.26/codestore-v1.18.26-darwin-x64.tar.gz"
-  version "1.18.26"
-  sha256 "b367e5b943fcdc41a233130bfcd7c0e2f38f562c2af27cf2a6d9daa92c32ef06"
+  url "https://s3.code.store/codestore-v1.18.27/codestore-v1.18.27-darwin-x64.tar.gz"
+  version "1.18.27"
+  sha256 "de7c7a362c52d13d5932db55c670e95fa8903231808cf9df91afbdd5da4a2901"
   license "MIT"
   depends_on "node@12"
 
